6c3c20e7fb0a4de0934c8178d66f21ee1f26fba3
devmohidul <mohidulhasan145@gmail.com>
2025-04-29 23:07:40 +0600

    React Practice

---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <img
+      style={{ marginRight: "10px" }}
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
